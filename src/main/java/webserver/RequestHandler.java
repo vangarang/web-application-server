a1593871841071989448047440e0d2502b5decfd
choinamwoo <vangarang@naver.com>
2019-06-16 18:24:46 +0900

    20190616 3.4.3.1 요구사항1 - index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,29 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	String line = "";
+        	String[] tokens = null;
+        	String url = "";
+        	//190616 1) BufferedReader 추가: InputStream을 한 줄 단위(readLine)로 읽기 위해 사용
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	do{
+        		line = br.readLine();
+        		if (line==null) {return;}
+        		
+        		//190616 2) 문자열 분리
+        		tokens = line.split(" ");
+        		
+        		//190616 3) 요청 URL에 해당하는 파일 읽어 전달
+        		if (tokens[0].equals("GET") ) {
+        			url = tokens[1];
+        		}
+        	}while(!"".equals(line));
+        	
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
+            if(!"".equals(url)) {
+            	body = Files.readAllBytes(new File("./webapp"+url).toPath()); 
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
